171b5c24e493c7f2ce29dd857b79e1b641f45e0c
bytesandroses <isaacabodunrin05@gmail.com>
2024-07-21 01:42:03 +0200

    Map AuthorDto to EditAuthorViewModel; and EditAuthorViewModel to UpdateAuthorDto

--- src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
+++ src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
@@ -8,8 +8,10 @@ public class BookStoreWebAutoMapperProfile : Profile
 {
     public BookStoreWebAutoMapperProfile()
     {
-        //Define your AutoMapper configuration here for the Web project.
         CreateMap<BookDto, CreateUpdateBookDto>();
         CreateMap<Pages.Authors.CreateModalModel.CreateAuthorViewModel, CreateAuthorDto>();
+        CreateMap<AuthorDto, Pages.Authors.EditModalModel.EditAuthorViewModel>();
+        CreateMap<Pages.Authors.EditModalModel.EditAuthorViewModel,
+                    UpdateAuthorDto>();
     }
 }
