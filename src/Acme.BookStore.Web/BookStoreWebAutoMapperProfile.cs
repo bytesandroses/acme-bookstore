547d5efe24529e35db6fe9c34a9ff63ceaa607d2
bytesandroses <isaacabodunrin05@gmail.com>
2024-07-21 00:51:03 +0200

    Create modal for Authors

--- src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
+++ src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
@@ -8,5 +8,11 @@ public class BookStoreWebAutoMapperProfile : Profile
     public BookStoreWebAutoMapperProfile()
     {
         CreateMap<BookDto, CreateUpdateBookDto>();
+        CreateMap<Pages.Authors.CreateModalModel.CreateAuthorViewModel, CreateAuthorDto>();
+        CreateMap<AuthorDto, Pages.Authors.EditModalModel.EditAuthorViewModel>();
+        CreateMap<Pages.Authors.EditModalModel.EditAuthorViewModel, UpdateAuthorDto>();
+        CreateMap<Pages.Books.CreateModalModel.CreateBookViewModel, CreateUpdateBookDto>();
+        CreateMap<BookDto, Pages.Books.EditModalModel.EditBookViewModel>();
+        CreateMap<Pages.Books.EditModalModel.EditBookViewModel, CreateUpdateBookDto>();
     }
 }
