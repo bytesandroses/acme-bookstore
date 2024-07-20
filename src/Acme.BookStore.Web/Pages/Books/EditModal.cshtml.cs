4235ab8eecb818d9db9145026b7e9803d24dff4c
bytesandroses <isaacabodunrin05@gmail.com>
2024-07-20 10:26:02 +0200

    Add back action button

--- src/Acme.BookStore.Web/Pages/Books/EditModal.cshtml.cs
+++ src/Acme.BookStore.Web/Pages/Books/EditModal.cshtml.cs
@@ -1,30 +1,35 @@
+using System;
 using System.Threading.Tasks;
 using Acme.BookStore.Books;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Acme.BookStore.Web.Pages.Books
+namespace Acme.BookStore.Web.Pages.Books;
+
+public class EditModalModel : BookStorePageModel
 {
-    public class EditModalModel : BookStorePageModel
+    [HiddenInput]
+    [BindProperty(SupportsGet = true)]
+    public Guid Id { get; set; }
+
+    [BindProperty]
+    public CreateUpdateBookDto Book { get; set; }
+
+    private readonly IBookAppService _bookAppService;
+
+    public EditModalModel(IBookAppService bookAppService)
     {
-        [BindProperty]
-        public CreateUpdateBookDto Book { get; set; }
+        _bookAppService = bookAppService;
+    }
 
-        private readonly IBookAppService _bookAppService;
+    public async Task OnGetAsync()
+    {
+        var bookDto = await _bookAppService.GetAsync(Id);
+        Book = ObjectMapper.Map<BookDto, CreateUpdateBookDto>(bookDto);
+    }
 
-        public CreateModalModel(IBookAppService bookAppService)
-        {
-            _bookAppService = bookAppService;
-        }
-
-        public void OnGet()
-        {
-            Book = new CreateUpdateBookDto();
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            await _bookAppService.CreateAsync(Book);
-            return NoContent();
-        }
+    public async Task<IActionResult> OnPostAsync()
+    {
+        await _bookAppService.UpdateAsync(Id, Book);
+        return NoContent();
     }
 }
