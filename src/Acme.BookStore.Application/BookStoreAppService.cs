8786887d7d048fd4d3a915565d32cb495c6358d4
bytesandroses <isaacabodunrin05@gmail.com>
2024-07-20 21:36:53 +0200

    Fix: BookAppService class was incorrectly defined inside BookStoreAppService.cs instead of BookAppService.cs. Both files corrected to allow program to compile

--- src/Acme.BookStore.Application/BookStoreAppService.cs
+++ src/Acme.BookStore.Application/BookStoreAppService.cs
@@ -1,27 +1,17 @@
 ﻿using System;
-using Acme.BookStore.Permissions;
-using Volo.Abp.Application.Dtos;
+using System.Collections.Generic;
+using System.Text;
+using Acme.BookStore.Localization;
 using Volo.Abp.Application.Services;
-using Volo.Abp.Domain.Repositories;
 
-namespace Acme.BookStore.Books;
+namespace Acme.BookStore;
 
-public class BookAppService :
-    CrudAppService<
-        Book, //The Book entity
-        BookDto, //Used to show books
-        Guid, //Primary key of the book entity
-        PagedAndSortedResultRequestDto, //Used for paging/sorting
-        CreateUpdateBookDto>, //Used to create/update a book
-    IBookAppService //implement the IBookAppService
+/* Inherit your application services from this class.
+ */
+public abstract class BookStoreAppService : ApplicationService
 {
-    public BookAppService(IRepository<Book, Guid> repository)
-        : base(repository)
+    protected BookStoreAppService()
     {
-        GetPolicyName = BookStorePermissions.Books.Default;
-        GetListPolicyName = BookStorePermissions.Books.Default;
-        CreatePolicyName = BookStorePermissions.Books.Create;
-        UpdatePolicyName = BookStorePermissions.Books.Edit;
-        DeletePolicyName = BookStorePermissions.Books.Delete;
+        LocalizationResource = typeof(BookStoreResource);
     }
 }
